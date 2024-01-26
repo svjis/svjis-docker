79f8d7772b517f5a82de6c114a5b91d7a0f751f7
Jaroslav Beran <jaroslav.beran@gmail.com>
2024-01-26 19:58:33 +0100

    Database host fix

--- docker-compose/create-schema.sh
+++ docker-compose/create-schema.sh
@@ -1,9 +1,10 @@
 USER=sysdba
 PASSWORD=change-it
+HOST=svjis-db
 DATABASE=SVJIS_PRODUCTION
 
-/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "localhost:$DATABASE"
+/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/database.sql' "$HOST:$DATABASE"
 rm /firebird/database.sql
 echo "EXECUTE PROCEDURE SP_CREATE_COMPANY 'New Company'; COMMIT;" > /firebird/comp.sql
-/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/comp.sql' "localhost:$DATABASE"
+/usr/local/firebird/bin/isql -user "$USER" -password "$PASSWORD" -input '/firebird/comp.sql' "$HOST:$DATABASE"
 rm /firebird/comp.sql
